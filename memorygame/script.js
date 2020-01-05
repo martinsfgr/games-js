7c08b426096e3c5463cce91af8607fd536accb93
Fagner <mauriciofagner2010@hotmail.com>
2020-01-05 05:26:47 -0300

    remove some notes from the game's initial logic

--- memorygame/script.js
+++ memorygame/script.js
@@ -1,20 +1,3 @@
-//mapeamento das variaveis globais:
-//  - nodelist das divs que conterão as imagens
-//  - lista das imagens que serão usadas nas divs
-//  - variável com o método concat para duplicar as imagens
-//  - variável com as imagens embaralhadas
-//  - variáveis vazias com a primeira e a segunda carta
-
-//mapeamento das funções:
-// - função para embaralhar as cartas
-// - função para revelar a carta
-// - função para esconder a carta
-// - função para iniciar a jogada
-// - função para executar a jogada
-// - função para verificar as cartas iguais
-// - função para travar os cliques (tratamento de erro)
-// - função para bloquear cliques em cartas encontradas
-
 let primeiraCarta;
 let segundaCarta;
 

b931e61877d760557caf8f2f3f49a965162fdfff
Fagner <mauriciofagner2010@hotmail.com>
2020-01-05 06:32:30 -0300

    add notes about the logical flow of functions

--- memorygame/script.js
+++ memorygame/script.js
@@ -96,3 +96,36 @@ for(carta of cartas){
 setTimeout(() => {
     iniciarJogada();
 }, 1000);
+
+
+// FLUXO DAS FUNÇÕES:
+
+// 1. O jogo vai deixar aberto todas as cartas através do for da linha 92
+//    e executando a função abrirCarta(); na linha 93.
+
+// 2. As cartas ficarão abertas apenas por 1 segundo, assim como é definido no
+//    setTimeout na linha 96, onde é executado a função iniciarJogada() que, por sua vez,
+//    define as duas cartas como indefinidas e chama a função bloquearEncontradas para ser executada.
+
+// 3. A função bloquearEncontradas() percorre toda a lista de cartas e verifica se
+//    elas já foram encontradas pelo jogador através da classe daquele elemento.
+//    No caso de ainda não ser encontrado, todas as cartas que satisfizerem essa condição
+//    serão escondidas através da função esconderCarta().
+
+// 4. Sendo chamada a função esconderCarta(), é dentro dela onde se começa de fato as
+//    ações determinadas pelo jogador, pois na linha 37 é definido que, no momento em que
+//    o jogador clicar em cima de alguma carta, a função executarJogada() é chamada.
+//    Ou seja, é importante notar que apenas as cartas que ainda não foram encontradas são clicáveis,
+//    afinal, se enquadram na função esconderCarta() apenas aquelas cartas que foram filtradas
+//    pela função bloquearEncontradas().
+
+// 5. Por sua vez, a função executarJogada() chama, primeiramente, a função abrirCarta,
+//    definindo como argumento de 'carta', o event.target, ou seja, aquele elemento que clicarmos.
+//    Após o jogador selecionar as duas cartas, o click é travado até que aa função 
+//    compararImagens() seja executada.
+
+// 6. A função compararImagens() vai verificar através do backgroundImage se aquelas cartas
+//    são pares. Caso sejam, os dois elementos terão uma classe adicionada e serão travadas
+//    pela função bloquearEncontradas() e, posteriormente, a função iniciarJogada() é chamada
+//    e todo processo se inicia novamente. Caso as cartas não sejam pares, elas são escondidas
+//    e a função iniciarJogada() é chamada e todo o processo se inicia novamente.
